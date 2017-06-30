223678eb8e3078e8809fe7e20b6e10d74227a6d6
halwu(吴浩麟) <halwu@tencent.com>
2017-06-30 18:03:41 +0800

    fix CI

--- test.js
+++ test.js
@@ -5,6 +5,8 @@ const webpack = execSync('npm run demo').toString();
 console.log(webpack);
 const change = execSync('git ls-files -m demo').toString();
 if (change.length > 0) {
-	console.error(`webpack compile out in demo has changed, review these files:\n${change}`);
+	console.error(`😡 webpack compile out in demo has changed, review these files:\n${change}`);
 	process.exit(1);
+} else {
+	console.info('😘 webpack compile out in demo not change, test pass~');
 }
