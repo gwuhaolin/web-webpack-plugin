9bfd408ac31590bc034cc2e889c70beaba6d09f8
吴浩麟 <wuhaolin@meituan.com>
2018-11-20 16:28:56 +0800

    feat(AutoPlugin): add entryPath to templateCompiler as third arg

---
+++ demo/auto-plugin-react-typescript/dist/signup.1845e32ed5ae26cd164c.js
@@ -0,0 +1,37 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["signup"],{
+
+/***/ "./src/pages/signup/index.css":
+/*!************************************!*\
+  !*** ./src/pages/signup/index.css ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/pages/signup/index.css?");
+
+/***/ }),
+
+/***/ "./src/pages/signup/index.tsx":
+/*!************************************!*\
+  !*** ./src/pages/signup/index.tsx ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar tslib_1 = __webpack_require__(/*! tslib */ \"../../node_modules/tslib/tslib.es6.js\");\nvar react_1 = __webpack_require__(/*! react */ \"../../node_modules/react/index.js\");\nvar React = __webpack_require__(/*! react */ \"../../node_modules/react/index.js\");\nvar react_dom_1 = __webpack_require__(/*! react-dom */ \"../../node_modules/react-dom/index.js\");\nvar Button_1 = __webpack_require__(/*! ../../components/Button */ \"./src/components/Button/index.tsx\");\n__webpack_require__(/*! ./index.css */ \"./src/pages/signup/index.css\");\nvar PageHome = /** @class */ (function (_super) {\n    tslib_1.__extends(PageHome, _super);\n    function PageHome() {\n        return _super !== null && _super.apply(this, arguments) || this;\n    }\n    PageHome.prototype.render = function () {\n        return (React.createElement(\"div\", null,\n            \"PageHome\",\n            React.createElement(Button_1.Button, null)));\n    };\n    return PageHome;\n}(react_1.Component));\nreact_dom_1.render(React.createElement(PageHome, null), document.getElementById('app'));\n\n\n//# sourceURL=webpack:///./src/pages/signup/index.tsx?");
+
+/***/ }),
+
+/***/ 5:
+/*!********************************!*\
+  !*** multi ./src/pages/signup ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__(/*! /Users/halwu/WebstormProjects/web-webpack-plugin/demo/auto-plugin-react-typescript/src/pages/signup */\"./src/pages/signup/index.tsx\");\n\n\n//# sourceURL=webpack:///multi_./src/pages/signup?");
+
+/***/ })
+
+},[[5,"runtime","common","base"]]]);
